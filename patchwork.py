b8ac14d8a6e7a9ec051322519f7d287ba3957dd9
Patrick Frampton <pframpton@salesforce.com>
2019-06-26 13:18:11 -0400

    log log log

--- patchwork.py
+++ patchwork.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
+from optparse import OptionParser
+import logging
 
 def main():
-  print("Hello World")
+  usage = "usage: %prog [options]"
+  parser = OptionParser(usage=usage)
+
+  parser.add_option("-l", "--level", dest="loglevel", help="Set log level to LOGLEVEL", default="INFO")
+
+  (options, _) = parser.parse_args()
+
+  numeric_level = getattr(logging, options.loglevel.upper())
+  if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % options.loglevel)
+
+  logging.basicConfig(level=numeric_level)
 
 if __name__ == "__main__":
   main()
