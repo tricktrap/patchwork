4eec7f02fd7b971a72284f203ebef5a466d3f3f8
Patrick Frampton <patrick.frampton@gmail.com>
2019-07-12 14:43:52 -0400

    Clean up main script

--- patchwork.py
+++ patchwork.py
@@ -10,11 +10,30 @@ def main():
 
   (options, _) = parser.parse_args()
 
-  numeric_level = getattr(logging, options.loglevel.upper())
-  if not isinstance(numeric_level, int):
-    raise ValueError('Invalid log level: %s' % options.loglevel)
+  configureLogging(options.loglevel.upper())
+  logger = logging.getLogger(__name__)
 
-  logging.basicConfig(level=numeric_level)
+  logger.info("Starting run")
+
+  # TODO Add the fun bits here.
+
+  logger.info("Complete")
+
+def configureLogging(level):
+  numeric_level = getattr(logging, level)
+  if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % level)
+
+  logger = logging.getLogger()
+  logger.setLevel(numeric_level)
+
+  # Use stdout/stderr
+  ch = logging.StreamHandler()
+
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  ch.setFormatter(formatter)
+
+  logger.addHandler(ch)
 
 if __name__ == "__main__":
   main()
